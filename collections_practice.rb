8b16a717b2750cf6ceedb7b29fae17c56ae8cfee
Anthony Rurup <tcrurup@gmail.com>
2019-04-28 04:32:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,3 +22,7 @@ def swap_elements_from_to(array, index, destination_index)
   swapped_array[destination_index] = array[index]
   swapped_array
 end
+
+def reverse_array(array)
+  array.reverse
+end
